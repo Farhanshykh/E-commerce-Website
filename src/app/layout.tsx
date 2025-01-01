d56e42c9c0c56934d69546565599f520ceddefd6
Farhan shykh <farhanshykh22k@gmail.com>
2025-01-01 10:57:28 -0800

    My E-commerce Website

---
+++ src/app/layout.tsx
@@ -0,0 +1,49 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { Poppins } from 'next/font/google';
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import favicon from "@/app/favicon.ico";
+const poppins_init = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-poppins',
+})
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Hackathon | E-commerce",
+  description: "Made by Farhan",
+  
+  
+
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${poppins_init.variable} antialiased`}
+      >
+         <Navbar/>
+        {children}
+        <Footer/>
+      </body>
+    </html>
+  );
+}
